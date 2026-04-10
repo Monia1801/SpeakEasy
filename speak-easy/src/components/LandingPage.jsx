f1f5666be33c8ca68bd1709316b2b06f8dd53633
monia <moniavoona18@gmail.com>
2026-04-10 21:52:15 +0530

    Completed Landing Page

--- speak-easy/src/components/LandingPage.jsx
+++ speak-easy/src/components/LandingPage.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import "../styles/LandingPage.css";
 
 const LandingPage = () => {
   return (
@@ -7,7 +8,8 @@ const LandingPage = () => {
       <header>
         <nav>
           <div className="nav-left-side">
-            <img src="" alt="logo"></img>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="audio-logo">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M12 18.75a6 6 0 0 0 6-6v-1.5m-6 7.5a6 6 0 0 1-6-6v-1.5m6 7.5v3.75m-3.75 0h7.5M12 15.75a3 3 0 0 1-3-3V4.5a3 3 0 1 1 6 0v8.25a3 3 0 0 1-3 3Z" /></svg>
             <h2>SpeakEasy</h2>
           </div>
 
@@ -28,16 +30,15 @@ const LandingPage = () => {
       <section id="home">
         <div className="home-left-side">
           <h1>Master English for Interviews & Visa Success</h1>
-          <h4>Practice speaking with AI and gain confidence faster. Get real time feedback on your pronunciation, fluency, and confidence. </h4>
+          <p>Practice speaking with AI and gain confidence faster. Get real time feedback on your pronunciation, fluency, and confidence. </p>
           <div className="buttons">
             <button>Start Practicing</button>
             <button>Watch Demo</button>
           </div>
         </div>
 
-        <div id="logo">
-          <img src="" alt="logo"></img>
-        </div>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="home-audio-logo">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M12 18.75a6 6 0 0 0 6-6v-1.5m-6 7.5a6 6 0 0 1-6-6v-1.5m6 7.5v3.75m-3.75 0h7.5M12 15.75a3 3 0 0 1-3-3V4.5a3 3 0 1 1 6 0v8.25a3 3 0 0 1-3 3Z" /></svg>
 
       </section>
 
@@ -45,23 +46,23 @@ const LandingPage = () => {
       <section id="features">
         <div className="heading">
           <h2>Powerful Features</h2>
-          <h4>Everything you need to master English speaking skills</h4>
+          <p>Everything you need to master English speaking skills</p>
         </div>
 
         <div className="cards-container">
           <div className="card">
             <h3>Mock Interview</h3>
-            <h4>Practice answering real interview questions with AI-powered feedback on clarity and confidence.</h4>
+            <p>Practice answering real interview questions with AI-powered feedback on clarity and confidence.</p>
           </div>
 
           <div className="card">
             <h3>Tongue Twisters</h3>
-            <h4>Fun exercises to improve pronunciation accuracy and speaking speed with instant results.</h4>
+            <p>Fun exercises to improve pronunciation accuracy and speaking speed with instant results.</p>
           </div>
 
           <div className="card">
             <h3>Word Pronunciation</h3>
-            <h4>Learn correct pronunciation with phonetics, audio playback, and voice comparison tools.</h4>
+            <p>Learn correct pronunciation with phonetics, audio playback, and voice comparison tools.</p>
           </div>
         </div>
       </section>
@@ -70,33 +71,33 @@ const LandingPage = () => {
       <section id="future-updates">
         <div className="heading">
           <h2>Coming Soon</h2>
-          <h4>Level up your communication with advanced AI features</h4>
+          <p>Level up your communication with advanced AI features</p>
         </div>
 
         <div className="cards-container">
           <div className="card">
             <h3>AI Interview Questions</h3>
-            <h4>Dynamic AI-generated questions based on role</h4>
+            <p>Dynamic AI-generated questions based on role</p>
           </div>
 
           <div className="card">
             <h3>Grammar Correction</h3>
-            <h4>Real-time sentence correction</h4>
+            <p>Real-time sentence correction</p>
           </div>
 
           <div className="card">
             <h3>Confidence Score</h3>
-            <h4>Analyze tone and confidence</h4>
+            <p>Analyze tone and confidence</p>
           </div>
 
           <div className="card">
             <h3>Daily Streak</h3>
-            <h4>Track your consistency</h4>
+            <p>Track your consistency</p>
           </div>
 
           <div className="card">
             <h3>Dark Mode</h3>
-            <h4>Switch themes for comfort</h4>
+            <p>Switch themes for comfort</p>
           </div>
         </div>
       </section>
@@ -105,7 +106,7 @@ const LandingPage = () => {
       <section className="conclusion">
         <div className="conc-card">
           <h2>Start your speaking journey today </h2>
-          <h4>Join thousands of learners improving their English speaking skills every day.</h4>
+          <p>Join thousands of learners improving their English speaking <br></br> skills every day.</p>
           <button>Get Started</button>
         </div>
       </section>
